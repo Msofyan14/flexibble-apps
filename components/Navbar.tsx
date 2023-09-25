ada3a49f42389b2f2fc8634c8121151422743994
Msofyan14 <mxsofyan14@gmail.com>
2023-09-25 23:37:27 +0700

    authentification

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,12 @@ import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -22,10 +25,13 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
-            <Link href="/create-project">Share Work</Link>
+            <Link href="/create-project">
+              <Button title="Share work" />
+            </Link>
+
+            <ProfileMenu session={session} />
           </>
         ) : (
           <AuthProviders />
